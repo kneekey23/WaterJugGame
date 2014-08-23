68f596a606cd753b67be2d1daf0477de52068952
Nicki Klein <nicoleklein@me.com>
2014-08-23 16:28:06 -0700

    The Water Jug Game

---
+++ H20Games/Scripts/References.js
@@ -0,0 +1,11 @@
+﻿/// <autosync enabled="false" />
+
+/// <reference path="jquery-2.1.1.js" />
+
+/// <reference path="jquery-2.1.1.intellisense.js" />
+
+/// <reference path="jquery.validate.js" />
+
+
+/// <reference path="bootstrap.js" />
+
---
+++ H20Games/Scripts/WaterJavaScript.js
@@ -0,0 +1,18 @@
+﻿//create namespaces to organize functions
+
+var water = {
+    page: {},
+    layout: {}
+};
+
+water.startupLayout = function () {
+    if (water.startup) {
+        water.startup();
+    }
+
+}
+
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
