c1d4a99642e59a1a69b865dacee301914451e3ff
Nicki Klein <nicoleklein@me.com>
2014-08-30 09:04:28 -0700

    updates

--- H20Games/Scripts/WaterJavaScript.js
+++ H20Games/Scripts/WaterJavaScript.js
@@ -12,7 +12,17 @@ water.startupLayout = function () {
 
 }
 
-$.urlParam = function (name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results[1] || 0;
+
+water.layout.getQueryParams = function(qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
 }
